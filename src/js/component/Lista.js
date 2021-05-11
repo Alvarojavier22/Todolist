5f8855c6141907af1104b0b688125986599696e8
Alvaro Javier <alvarojavierchagas@hotmail.com>
2021-05-11 14:54:53 +0000

    pronta

---
+++ src/js/component/Lista.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+const Lista = () => {
+	const [tarea, setTarea] = useState();
+	const [listaTareas, setListaTareas] = useState([]);
+
+	const añadirTarea = event => {
+		event.preventDefault();
+		setListaTareas([...listaTareas, tarea]);
+		setTarea("");
+	};
+
+	const eliminar = i => {
+		let nuevoarraytareas = listaTareas.filter((element, index) => {
+			if (i != index) {
+				return element;
+			}
+		});
+		setListaTareas(nuevoarraytareas);
+	};
+
+	return (
+		<div>
+			<form onSubmit={añadirTarea}>
+				<div className="d-flex form-row align-items-center">
+					<div className="col-auto">
+						<label className="sr-only" htmlFor="inlineFormInput">
+							Name
+						</label>
+						<input
+							type="text"
+							className="form-control mb-2"
+							id="inlineFormInput"
+							placeholder="Agregar tareas"
+							onChange={event => setTarea(event.target.value)}
+						/>
+					</div>
+					<div className="col-auto">
+						<button type="submit" className="btn btn-success mb-2">
+							Agregar tarea
+						</button>
+					</div>
+				</div>
+			</form>
+			<div className="lista">
+				<ul className="list-group sm">
+					{listaTareas.map((element, index) => {
+						return (
+							<li
+								className="btn btn-outline-success mb-3"
+								key={index}>
+								{element}
+								<button
+									className="btn"
+									onClick={() => eliminar(index)}>
+									<i className="fas fa-arrow-alt-circle-down fa-2x"></i>
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+
+			<div>
+				<p>
+					{listaTareas.length == 0
+						? "No hay tareas"
+						: "Hay tareas " + listaTareas.length}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Lista;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,11 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Lista from "./Lista";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Lista />
 		</div>
 	);
 }
